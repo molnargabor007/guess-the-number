9a68881e8ac91361aee078fe2134b354943c9413
Gábor Molnár <atoderma@gmail.com>
2018-02-06 22:55:35 +0100

    guess game created

--- guess_game.py
+++ guess_game.py
@@ -1,1 +1,11 @@
-print "Hello World"
+secret = 15
+
+guess = int(raw_input("Guess the number between 1 and 30: ").strip())
+
+if guess == secret:
+    print "Congratulations! You WIN! the number is: ", guess
+
+while guess != secret:
+    guess = int(raw_input("Sorry, that is not my number, try agian: ").strip())
+    if guess == secret:
+        print "Congratulations! You WIN! the number is: ", guess
